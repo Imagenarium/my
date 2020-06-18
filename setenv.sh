2bd7d1e29bbdd9cbee0715af92e307fd907b17e7
root <root@OPENNTB0018.crpt.local>
2020-06-18 17:30:55 +0300

    update version

--- setenv.sh
+++ setenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # specify Imagenarium version
-VERSION="3.0.0.RC1"
+VERSION="3.1.0.RC1"
 
 # specify additional features
 FEATURES="gc"
@@ -13,7 +13,7 @@ VOLUME_GRACE_PERIOD=10080
 NETWORK_GRACE_PERIOD=1440
 
 # specify heap size for Cluster Control
-JAVA_OPTS=-Xmx256m
+JAVA_OPTS=-Xmx512m
 
 # specify heap size for agents
 AGENT_JAVA_OPTS=-Xmx64m
