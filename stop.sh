3bcc7a3c50028df287c28ed0aad57d878b1ce6d5
Вова <man4j@ya.ru>
2023-02-08 11:04:31 +0300

    fixes

--- stop.sh
+++ stop.sh
@@ -2,42 +2,25 @@
 
 set +e
 
-echo "Stopping Cluster Control..."
-docker service rm clustercontrol &> /dev/null
-echo "Done"
 
-echo "Stopping Console Agent..."
-docker service rm $(docker service ls --filter name=console-agent -q) &> /dev/null
-echo "Done"
+function stopService {
+  echo "Stopping $1..."
 
-echo "Stopping File Agent..."
-docker service rm $(docker service ls --filter name=file-agent -q) &> /dev/null
-echo "Done"
+  if [[ $(docker service ls -q --filter name=$1 | wc -l) != "0" ]]; then
+    docker service rm $(docker service ls --filter name=$1 -q) &> /dev/null
+  fi
 
-echo "Stopping Auth sync..."
-docker service rm authsync-agent &> /dev/null
-echo "Done"
+  sleep 2
+  echo "Done"
+}
 
-echo "Stopping Healthchecker..."
-docker service rm healthchecker-agent &> /dev/null
-echo "Done"
-
-echo "Stopping Docker GC..."
-docker service rm dockergc-agent &> /dev/null
-echo "Done"
-
-echo "Stopping Swarm Proxy..."
-docker service rm swarmproxy-agent &> /dev/null
-echo "Done"
-
-echo "Stopping StandBy..."
-docker service rm standby-agent &> /dev/null
-echo "Done"
-
-echo "Stopping Volume Controller..."
-docker service rm volumecontroller &> /dev/null
-echo "Done"
-
-echo "Stopping Volume Agent..."
-docker service rm volume-agent &> /dev/null
-echo "Done"
+stopService "clustercontrol"
+stopService "console-agent"
+stopService "file-agent"
+stopService "authsync-agent"
+stopService "healthchecker-agent"
+stopService "dockergc-agent"
+stopService "swarmproxy-agent"
+stopService "standby-agent"
+stopService "volumecontroller"
+stopService "volume-agent"
